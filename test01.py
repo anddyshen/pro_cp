497b8d1af0a7ed891b57e2158a8e4f0735abbf7c
anddyshen <54139753+anddyshen@users.noreply.github.com>
2019-09-25 18:28:38 +0800

    奖项对比 test01

--- test01.py
+++ test01.py
@@ -1,10 +1,10 @@
 
 rewards_ball_t = [['2019110', '01', '18', '22', '26', '27', '28', '08'], ['2019109', '03', '06', '07', '17', '28', '31', '10'], ['2019108', '07', '13', '19', '22', '25', '32', '13']]
 
-my_ball_list_ex0 = [['06', '20', '26', '17', '33', '12'], ['08']]
-my_ball_list_ex1 = [['02', '06' ,'15','28'],['10']]
-my_ball_list_ex2 = [['12', '23', '06'], ['']]
-my_ball_list_ex2 = [['2019110', '01', '18', '22', '26', '27', '28', '08'], ['2019109', '03', '06', '07', '17', '28', '31', '10'], ['2019108', '07', '13', '19', '22', '25', '32', '13']]
+m0 = my_ball_list_ex0 = [['06', '20', '26', '17', '33', '12'], ['08']]
+m1 = my_ball_list_ex1 = [['02', '06' ,'15','28'],['10']]
+m2 = my_ball_list_ex2 = [['12', '23', '06'], ['']]
+m3 = my_ball_list_ex2 = [['01', '18', '22', '26', '27', '28'], ['08']], [['12', '23', '06'], ['']], [['01', '18', '22', '26', '27', '28'], ['05']],[['01', '18', '22', '26', '27'], ['08']],[['03', '06', '07', '17', '28', '31'], ['10']], [['07', '13', '19', '22', '25', '32'], ['13']]
 
   
 def rewards_level(red_ball,blue_ball):
@@ -25,31 +25,54 @@ def rewards_level(red_ball,blue_ball):
         rewards_level = 2
     if red_ball == 6 and blue_ball == 1:
         rewards_level = 1
-
+    
     return rewards_level
 
 
 def rewards_cal(rewards_ball,my_ball):
     red_ball = 0
     blue_ball = 0
-    re_level = 0
+    award_results = []
     match_count = 0
-    for i in range(0,len(my_ball)):
-        for k in range(0,len(rewards_ball)):
-            for j in range(0,8):
-                if my_ball[0][i] == rewards_ball[k][j]:
+    awards_count = 0
+    sNo = ""
+    award_results = []
+    award_results_t = []
+    award_results_t1 = []
+    for i in range(0,len(rewards_ball)):#总选择的中奖历史号码的注数，每一注都是标准，每个号码都去对一遍
+        award_results_t1 = rewards_ball_t[i]
+        for j in range(0,len(my_ball)):#总输入的所有号码注数，有几注对几次 
+            my_ball_t = my_ball[j]
+            #for k in range(0,len(my_ball_t)):#取出第j(一注)标准答案，分割这个标注答案
+            #my_ball_split = my_ball_t[1]
+            for m in range(0,len(my_ball_t[0])):#分拆当前自选注
+                if my_ball_t[0][m] == award_results_t1[m+1]:
                     red_ball += 1
-                if my_ball[1][0] == rewards_ball[k][7]:
+                if my_ball_t[1][0] == award_results_t1[7]:
                     blue_ball = 1
+                
+                if red_ball >= 4 or blue_ball == 1:
+                    sNo = award_results_t1[0]
+
+                a = rewards_level(red_ball,blue_ball)
+                awards_count += 1
+            red_ball = 0
+            blue_ball = 0
+
+            award_results_t.append(sNo)
+            award_results_t.append(a)
     
-        rewards_level(red_ball,blue_ball)
-        match_count += 1
+        award_results.append(award_results_t)
 
-    return re_level,match_count
+#match_count += 1    
+
+    return award_results 
+    
 
 
 
+a = rewards_cal(rewards_ball_t,m3)
 
-a = rewards_cal(rewards_ball_t,my_ball_list_ex0)
+print(f"中奖结果列表：{a} \n\n")
 
-print(a)
+print(f"你所选的号码在过去的 {len(rewards_ball_t)} 期中，")#中了 {a[0]} 注 {a[1]} 等奖")
